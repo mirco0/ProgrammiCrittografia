aa342f4802a0825d0873e7ebc21f92d2f6b0f6ff
mirco0 <mircomicromac@gmail.com>
2025-11-09 15:48:10 +0100

    generazione tabella NL

--- CrittoEsercizi2/Esercizio 5/analisi_spn.py
+++ CrittoEsercizi2/Esercizio 5/analisi_spn.py
@@ -18,6 +18,18 @@ class CrittoAnalisiSPN:
         [1,1,1,1,0,1,1,1]
     ]
 
+    def calcola_NL_table(self, sbox):
+        table = []
+        variables = len(sbox[0]) // 2
+        for a in range(2**variables):
+            a_arr = [int(x) for x in list(f'{a:0{variables}b}')]
+            line = []
+            for b in range(2**variables):
+                # Workaround prende la rappresentazione binaria e la trasforma in array
+                b_arr = [int(x) for x in list(f'{b:0{variables}b}')]
+                line.append(self.calcola_NL(a_arr,b_arr))
+            table.append(line)
+        return table
 
     def calcola_NL(self,i,j):
         NL = 0
@@ -30,6 +42,4 @@ class CrittoAnalisiSPN:
         return NL
 
 cr = CrittoAnalisiSPN()
-i = [0,1,0,1]
-j = [0,0,0,1]
-print(cr.calcola_NL(i,j))
+print(cr.calcola_NL_table(cr.inp))
