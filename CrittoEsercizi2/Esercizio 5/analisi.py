bb5075e876dc7dfea581ee148deeac901cfdb9df
mirco0 <mircomicromac@gmail.com>
2025-11-11 21:45:58 +0100

    soluzione punto a

---
+++ CrittoEsercizi2/Esercizio 5/analisi.py
@@ -0,0 +1,50 @@
+#Codice per eseguire la crittoanalisi
+
+from analisi_spn import CrittoAnalisiSPN
+from perm import perm
+
+cr = CrittoAnalisiSPN()
+
+# Creo oggetto `perm` con la permutazione dall'esercizio 
+PI_S_Primo = {
+    '0':'8',
+    '1':'4',
+    '2':'2',
+    '3':'1',
+    '4':'C',
+    '5':'6',
+    '6':'3',
+    '7':'D',
+    '8':'A',
+    '9':'5',
+    'A':'E',
+    'B':'7',
+    'C':'F',
+    'D':'B',
+    'E':'9',
+    'F':'0',
+}
+
+PS1 = {}
+for a in PI_S_Primo.items():
+    k = int(a[0],16)
+    v = int(a[1],16)
+    PS1[k] = v
+
+
+PI_S = perm()
+PI_S.set_key(PS1)
+
+table = cr.genera_SBOX_table(PI_S)
+
+# Tabella SBOX in latex
+header=["$X_1$","$X_2$","$X_3$","$X_4$","$Y_1$","$Y_2$","$Y_3$","$Y_4$"]
+cr.latex_table(table,name="perm_table.tex",header=header)
+
+#Tabella linear approximation in latex
+linear_app = cr.calcola_NL_table(table)
+header=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+side=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+cr.latex_table(linear_app,name="linear_approximation.tex",header=header,side=side)
+
+
--- CrittoEsercizi2/Esercizio 5/analisi_spn.py
+++ CrittoEsercizi2/Esercizio 5/analisi_spn.py
@@ -1,23 +1,21 @@
-class CrittoAnalisiSPN:
-    inp = [
-        [0,0,0,0,1,1,1,0],
-        [0,0,0,1,0,1,0,0],
-        [0,0,1,0,1,1,0,1],
-        [0,0,1,1,0,0,0,1],
-        [0,1,0,0,0,0,1,0],
-        [0,1,0,1,1,1,1,1],
-        [0,1,1,0,1,0,1,1],
-        [0,1,1,1,1,0,0,0],
-        [1,0,0,0,0,0,1,1],
-        [1,0,0,1,1,0,1,0],
-        [1,0,1,0,0,1,1,0],
-        [1,0,1,1,1,1,0,0],
-        [1,1,0,0,0,1,0,1],
-        [1,1,0,1,1,0,0,1],
-        [1,1,1,0,0,0,0,0],
-        [1,1,1,1,0,1,1,1]
-    ]
+from perm import perm
 
+#TODO: Cambiare implementazione per lavorare con stringhe binarie
+class CrittoAnalisiSPN:
+
+    def genera_SBOX_table(self,perm):
+        table = []
+        size = len(perm.domain)
+        for i in range(size):
+            line = []
+            start = [int(x) for x in list(f'{i:0{4}b}')]
+            c = perm.get_item(i)
+            end = [int(x) for x in list(f'{c:0{4}b}')]
+            line.extend(start)
+            line.extend(end)
+            table.append(line)
+        return table
+    
     def calcola_NL_table(self, sbox):
         table = []
         variables = len(sbox[0]) // 2
@@ -27,19 +25,50 @@ class CrittoAnalisiSPN:
             for b in range(2**variables):
                 # Workaround prende la rappresentazione binaria e la trasforma in array
                 b_arr = [int(x) for x in list(f'{b:0{variables}b}')]
-                line.append(self.calcola_NL(a_arr,b_arr))
+                line.append(self.calcola_NL(a_arr,b_arr,sbox))
             table.append(line)
         return table
 
-    def calcola_NL(self,i,j):
+    def calcola_NL(self,i,j,sbox_table):
         NL = 0
-        for line in self.inp:
+        for line in sbox_table:
             m = 1
             for index in range(len(i)):
                 m += i[index] * line[index]
                 m+= j[index] * line[index+len(i)]
             NL += (m % 2)
         return NL
+    
+    def latex_table(self, table, header=[], side=[], name="table.tex"):
 
-cr = CrittoAnalisiSPN()
-print(cr.calcola_NL_table(cr.inp))
+        n = len(table)
+        cols = len(table[0]) if table else 0
+        try:
+            with open(f"../latex/{name}", "x",encoding="utf-8") as f:
+
+                total_cols = cols + (1 if side else 0)
+                f.write(f"\\begin{{table}}[h]\n\t\\centering\n")
+                
+                f.write(f"\t\\begin{{tabular}}{{|*{{{total_cols}}}{{c|}}}}\n")
+                f.write("\t\t\\hline\n")
+
+                # Scrivi titoli
+                if header:
+                    if side:
+                        f.write("\t\t & " + " & ".join(header) + " \\\\\n")
+                    else:
+                        f.write("\t\t"+"& ".join(header) + " \\\\\n")
+                    f.write("\t\t\\hline\n")
+
+                #Scrivi contenuto
+                for i, row in enumerate(table):
+                    if side:
+                        f.write(f"\t\t{side[i]} & " + " & ".join(str(x) for x in row) + " \\\\\n")
+                    else:
+                        f.write("\t\t" + "& ".join(str(x) for x in row) + " \\\\\n")
+                    f.write("\t\t\\hline\n")
+
+                f.write("\t\\end{tabular}\n")
+                f.write("\\end{table}\n")
+        except:
+            print(f"File '/latex/{name}' esistente")
