457be0f1194f4a264716c3c900a2c5f92da2d712
mirco0 <mircomicromac@gmail.com>
2025-12-02 16:34:30 +0100

    aggiunta funzione di fattorizzazione

---
+++ CrittoEsercizi3/Esercizio 1/PoligHellman.py
@@ -0,0 +1,45 @@
+from Shanks import algo_shanks
+#Prende un intero n e restituisce un dizionario con 
+# chiave = fattore primo    (q)
+# valore = esponente        (c)
+
+N = {}
+
+def fattorizzazione(n):
+    if n == 1:
+        return {}
+    
+    if n in N:
+        return dict(N[n])
+    else:
+        fattori = None
+        for i in range(2,int(n**(1/2))+1):
+            if n % i == 0:
+                fattori = fattorizzazione(n//i)
+
+                f = fattorizzazione(i)
+                for k in f.items():
+                    fattori[k[0]] = fattori.get(k[0],0) + k[1]
+                # fattori[i] = fattori.get(i,0) + 1
+        if fattori == None:
+            fattori = {n:1}
+        
+        N[n] = dict(fattori)
+        return fattori
+
+def algo_pohlig_hellman(G,n,a,b,q,c):
+    B = [ 0 for _ in range(c)]
+    B[0] = b
+    for j in range(c):
+        delta = G.exp(B[j],n/G.exp(q,j+1))
+        #Trova i tale che delta = alpha^in/q con l'algoritmo di shanks
+        i = algo_shanks(G,G.exp(a,n/q),delta)
+
+        A[j] = i
+        B[j+1] = B[j] * G.exp(a,-A[j]*G.exp(q,j))
+    return A
+
+# def pohlig_algo_(G,n,a,b):
+#     fattori = fattorizzazione(n)
+    # for():
+        
